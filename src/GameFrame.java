ee21d7e5527eb09affdebb4c7e6c4fb1bd6be70f
KlaporeDevs <verbojanrich20@gmail.com>
2024-04-08 20:45:50 +0800

    Updated BrahaBraha

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.*;
 public class GameFrame extends JFrame{
     JButton Start;
     JButton Settings;
     JButton Exit;
+    private boolean fullscreenchecked = false;
+    private boolean timerchecked = false;
+    private int musicvolume = 50;
+    private int soundvolume = 50;
     GameFrame(){
         //Buttons
         Start = new JButton("Start");
@@ -67,29 +71,67 @@ public class GameFrame extends JFrame{
     private void showSettingsDialog(){
         //Settings Contents
         JDialog settingsDialogs = new JDialog(this, "Settings", true);
-        settingsDialogs.setSize(300, 250);
+        settingsDialogs.setSize(300, 300);
         settingsDialogs.setLayout(null);
         settingsDialogs.setLocationRelativeTo(this);
         //Components
         JCheckBox fullscreen = new JCheckBox("FullScreen");
         fullscreen.setBounds(20, 20, 150, 30);
+        fullscreen.setSelected(fullscreenchecked);
+        fullscreen.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                fullscreenchecked = fullscreen.isSelected();
+            }
+        });
         JCheckBox timer = new JCheckBox("Timer On/Off");
         timer.setBounds(20, 60, 150, 30);
-        JLabel musiclabel = new JLabel("Music Volume");
-        musiclabel.setBounds(20, 100, 150, 30);
-        JSlider music = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        timer.setSelected(timerchecked);
+        timer.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                timerchecked = timer.isSelected();
+            }
+        });
+        JLabel musics = new JLabel("Music Volume");
+        musics.setBounds(20, 100, 150, 30);
+        JSlider music = new JSlider(JSlider.HORIZONTAL, 0, 100, musicvolume);
         music.setBounds(20, 130, 200, 30);
+        music.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                musicvolume = music.getValue();
+            }
+        });
         JLabel sounds = new JLabel("Sound Volume");
         sounds.setBounds(20, 160, 200, 30);
-        JSlider sound = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        JSlider sound = new JSlider(JSlider.HORIZONTAL, 0, 100, soundvolume);
         sound.setBounds(20, 190, 200, 30);
+        sound.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                soundvolume = sound.getValue();
+            }
+        });
+        //Save Settings
+        JButton save = new JButton("Save Settings");
+        save.setBounds(100, 230, 120, 30);
+        //save button config
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(settingsDialogs, "Settings Saved");
+                settingsDialogs.dispose();
+            }
+        });
         //add to settings parts
         settingsDialogs.add(fullscreen);
         settingsDialogs.add(timer);
         settingsDialogs.add(music);
-        settingsDialogs.add(musiclabel);
+        settingsDialogs.add(musics);
         settingsDialogs.add(sound);
         settingsDialogs.add(sounds);
+        settingsDialogs.add(save);
         //Visibility
         settingsDialogs.setVisible(true);
     }
